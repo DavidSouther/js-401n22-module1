311705d373703141562764fba4c30aa2d8e8bb09
David Souther <davidsouther+github@gmail.com>
2022-12-06 19:30:02 -0800

    Added person route

--- src/server.js
+++ src/server.js
@@ -10,4 +10,12 @@ server.get('/hello', (_, res) => res.send('Hello!'));
 // When you say goodbye, you recieve "nailed it"
 server.get('/goodbye', (_, res) => res.send('Nailed It!'));
 
+server.get('/person', (req, res) => {
+  if (req.query.name) {
+    res.status(200).send({ name: req.query.name });
+  } else {
+    res.status(500).send();
+  }
+});
+
 module.exports = server;
